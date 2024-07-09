63e12e150ad3503a85b5961e59dcbcab48702284
Antonino <drakon9927@libero.it>
2024-07-09 11:15:49 +0200

    error

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -30,3 +30,13 @@
         <button type="submit">Add Comic</button>
     </form>
 @endsection
+
+@if ($errors->any())
+    <div>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
